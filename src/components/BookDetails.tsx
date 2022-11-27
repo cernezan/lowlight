44993e20cf30d0a3d9878fa51a40f7490311bedb
cernezan <mail@cernezan.com>
2022-11-27 10:53:31 +0100

    Adding highlights to Craft page

--- src/components/BookDetails.tsx
+++ src/components/BookDetails.tsx
@@ -1,6 +1,15 @@
 import * as React from "react"
 
 export default function BookDetails({ bookData, changePageFunction }: any) {
+
+    async function insertHighlight(highlight: string) {
+        const highlightBlock = craft.blockFactory.textBlock({
+            content: highlight,
+        })
+
+        const result = await craft.dataApi.addBlocks([highlightBlock])
+    }
+
     return (
         <div style={{ border: '1px solid red' }}>
             <button onClick={() => changePageFunction('booksList')}>Back</button>
@@ -12,6 +21,7 @@ export default function BookDetails({ bookData, changePageFunction }: any) {
                     <>
                         <div>Highlight: {highlight.bookHighlight}</div>
                         <div>Date: {highlight.highlightDate}</div>
+                        <button onClick={() => insertHighlight(highlight.bookHighlight)}>Insert Highlight</button>
                     </>
                 )
             })}
--- src/index.ts
+++ src/index.ts
@@ -1,71 +1,80 @@
 import { initApp } from './app'
 import './style.css'
+import { v4 } from 'uuid'
 
 initApp()
 
 async function saveObjectsToDB(objects: any) {
-    let booksKeys = []
-    for(let objectName in objects) {
-      booksKeys.push(objectName)
-      craft.storageApi.put(objectName, JSON.stringify(objects[objectName]))
-    }
-    craft.storageApi.put('booksKeys', JSON.stringify(booksKeys))
+  let booksKeys = []
+  for (let objectName in objects) {
+    const generatedID = v4()
+    booksKeys.push(generatedID)
+    craft.storageApi.put(generatedID, JSON.stringify(objects[objectName]))
+  }
+  craft.storageApi.put('booksKeys', JSON.stringify(booksKeys))
 }
 
-export async function  fileProcessor(event: any) {
-    const file = event.target.result
-    let highlights = file.split('==========')
-    let bookStore: any = {}
-    for(let highlight of highlights) {
-      const hightlightParts = highlight.split('\n').filter((part: string) => part !== '')
-      let bookTitle: string = ""
-      let bookAuthor: string = ""
-      let highlightDate: string = ""
-      let bookHighlight: string = ""
-  
-      hightlightParts.forEach((line: any, index: number) => {
-  
-        if(line == '')
-          return
-        // AUTHOR AND BOOK TITLE LINE
-        if(line.includes('(')) {
-          const split = line.split('(')
-          bookTitle = split[0].trim()
-          bookAuthor = split[1].slice(0, -1)
-        }
-        // META DATA LINE - PAGE, LOCATION, DATE ADDED
-        if(line.includes('- Your Highlight')) {
-          const split: string[]  = line.split('|')
-          const dateNonformated: Date = new Date(split[2].slice(10))
-          highlightDate = formatDateToSave(dateNonformated)
-        }
-        if(index == 2) {
-          bookHighlight = line
-        }
-      })
-  
-      if(bookTitle === "")
-        continue
-      
-      if(bookStore[bookTitle] === undefined) {
-        bookStore[bookTitle] = {
-          bookTitle: bookTitle,
-          bookAuthor: bookAuthor,
-          highlights: [{
-            highlightDate: highlightDate,
-            bookHighlight: bookHighlight
-          }]
-        }
-      } else {
-        bookStore[bookTitle].highlights.push({
+export async function fileProcessor(event: any) {
+  const file = event.target.result
+  let highlights = file.split('==========')
+
+  let bookStore: any = {}
+
+  for (let highlight of highlights) {
+    const hightlightParts = splitHighlights(highlight)
+    let bookTitle: string = ""
+    let bookAuthor: string = ""
+    let highlightDate: string = ""
+    let bookHighlight: string = ""
+
+    hightlightParts.forEach((line: any, index: number) => {
+
+      if (line == '')
+        return
+      // AUTHOR AND BOOK TITLE LINE
+      if (line.includes('(')) {
+        const split = line.split('(')
+        bookTitle = split[0].trim()
+        bookAuthor = split[1].slice(0, -1)
+      }
+      // META DATA LINE - PAGE, LOCATION, DATE ADDED
+      if (line.includes('- Your Highlight')) {
+        const split: string[] = line.split('|')
+        const dateNonformated: Date = new Date(split[2].slice(10))
+        highlightDate = formatDateToSave(dateNonformated)
+      }
+      if (index == 2) {
+        bookHighlight = line
+      }
+    })
+
+    if (bookTitle === "")
+      continue
+
+    if (bookStore[bookTitle] === undefined) {
+      bookStore[bookTitle] = {
+        bookTitle: bookTitle,
+        bookAuthor: bookAuthor,
+        highlights: [{
           highlightDate: highlightDate,
           bookHighlight: bookHighlight
-        })
+        }]
       }
+    } else {
+      bookStore[bookTitle].highlights.push({
+        highlightDate: highlightDate,
+        bookHighlight: bookHighlight
+      })
     }
-    saveObjectsToDB(bookStore)
   }
-  
+  saveObjectsToDB(bookStore)
+}
+
+// file processor functions 
+function splitHighlights(highlight: string) {
+  return highlight.split('\n').filter((part: string) => part !== '')
+}
+
 function formatDateToSave(date: Date) {
-    return `${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`
-  }
+  return `${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`
+}
