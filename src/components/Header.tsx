02b04981d7a5e914422b500b30164e0444291609
cernezan <mail@cernezan.com>
2022-11-28 19:41:51 +0100

    Small changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,17 @@
 import * as React from "react"
+import upload_icon from "../assets/icons/upload_icon.svg"
+
 
 export default function Header() {
     return (
         <div className="header">
+            <div style={{ display: 'flex', flex: '1' }}>
+                <button className="icon-btn">
+                    <img className="icon-btn-image" src={upload_icon} />
+                </button>
+            </div>
             <div className="header-text">Lowlight</div>
+            <div style={{ flex: '1' }}></div>
         </div>
     )
 }
