d547eb907a2685f4a99e0c608aaf0abd6d287499
cernezan <mail@cernezan.com>
2022-11-29 22:20:47 +0100

    Added react-icons, improved header and more

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,23 @@
 import * as React from "react"
-import upload_icon from "../assets/icons/upload_icon.svg"
+import { FiUpload } from "react-icons/fi"
 
 
 export default function Header() {
     return (
-        <div className="">
-            <div style={{ display: 'flex', flex: '1' }}>
-                <button className="icon-btn">
-                    <img className="icon-btn-image" src={upload_icon} />
-                </button>
+        <div className="flex w-full" style={{ maxHeight: '44px', minHeight: '44px' }}>
+            <div className="border-b-2 flex w-full">
+                {/* UPLAOD ICON */}
+                <div className="w-full left-0 flex">
+                    <button className="h-5 w-5 my-auto mx-4">
+                        <FiUpload className=" w-full h-full hover:text-gray-300" />
+                    </button>
+                </div>
+                {/* APP NAME  */}
+                <div className="w-full text-center my-auto text-xl font-bold">Lowlight</div>
+                {/* balance div */}
+                <div className="w-full"></div>
             </div>
-            <div className="text-3xl">Lowlight</div>
-            <div style={{ flex: '1' }}></div>
+
         </div>
     )
 }
